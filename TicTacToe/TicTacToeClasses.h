e705d8ee472424ebd787f9cc14c62b6f6042d541
M0rkeleb <wolfwood.sjc@gmail.com>
2017-01-05 22:04:29 -0500

    getFromSquare return
    Make sure getFromSquare returns (_) even if square is not valid.

--- TicTacToe/TicTacToeClasses.h
+++ TicTacToe/TicTacToeClasses.h
@@ -10,7 +10,7 @@ public:
 	void placeInSquare(std::size_t i, std::size_t j, char placed);
 	char getFromSquare(size_t i, size_t j)
 	{
-		if (i < m_dimension && j < m_dimension) { return boardContents[i][j]; }
+		if (i < m_dimension && j < m_dimension) { return boardContents[i][j]; } return '_';
 	}
 	friend std::ostream& operator<< (std::ostream &out, const TicTacToeBoard &tttBoard);
 	bool victoryReached();
